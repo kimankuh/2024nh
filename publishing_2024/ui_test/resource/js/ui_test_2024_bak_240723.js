0f73ef44d0c4036135c39c99dc604d251b54f5a3
송재연 <kimankuh@gmail.com>
2024-07-23 14:09:44 +0900

    바텀시트 높이값 및 코드 정리

--- publishing_2024/ui_test/resource/js/ui_test_2024_bak_240723.js
+++ publishing_2024/ui_test/resource/js/ui_test_2024_bak_240723.js
@@ -82,59 +82,87 @@ $(function(){
         
         e.preventDefault();
         $('.gnb_total').scrollTop(top2);
+
     });
 
 
 
     /*********************
-        draggable 바텀시트     
+        드래깅 바텀시트     
     **********************/    
-        var $bs = $('.dragging-bs');
-        var $handle = $('.drag-handle');
-        var wh = $(window).height();        
-        var hh = $('#headerLay').outerHeight();
-        var conh = $('.desc-area').outerHeight();
-        var bsh = wh - (hh + conh);
-        var bsMaxH = wh - hh;
-        var touchStart;
-        var touchLast;
-        
-        // init
-        $bs.height(bsh);
+        var wH = $(window).height();
+        // var hH = $('.header').outerHeight();
+        // var upH = $('.desc-area').outerHeight();
+        // var bsH = wH - (hH + upH);
+    
+        // console.log('화면 높이 : ', wH);
+        // console.log('헤더 높이 : ', hH);
+        // console.log('상단 높이 : ', upH);
+        // console.log('바텀 높이 : ', wH - (hH + upH));
+    
+        // $('.dragging-bs').height(bsH);
+    
+        var bottomSheet = $('.dragging-bs');
+        var bottomSheetH = $('.dragging-bs').outerHeight();
+        var bottomSheetH2 = $('.dragging-bs.active').outerHeight();
+        var draghandle = $('.drag-handle');
+        // var bottomTouchStart = 0;
+        // var bottomScrollStart;
+        var lastTouch;
+        var bsMaxH = wH - ($('#headerLay').outerHeight() + 50);
 
+        console.log('초기 바텀시트 높이 : ', bottomSheetH2)
+    
         // 초기 터치
-        $handle.on('touchstart', function(e){
-            touchStart = e.touches[0].pageY;
-            touchLast = 0;// touchmove시 저장된 값 초기화
+        draghandle.on('touchstart', function(e){
+            bottom_touch_start = e.touches[0].pageY;
+            // console.log('초기터치 : ', bottom_touch_start)
+    
+            lastTouch = 0;
         });
 
         //핸들을 터치한 경우 => 바텀시트 up
-        $handle.on('touchmove', function(e){
-            touchLast = e.touches[0].pageY;            
-            var gap = touchLast - touchStart;
-            var bsIngH = bsh + (-gap);
-            // console.log('마지막터치 : ', touchLast);            
+        draghandle.on('touchmove', function(e){
+            lastTouch = e.touches[0].pageY;
+            console.log('마지막터치 : ', lastTouch);
 
-            if($bs.hasClass('active')) return;// 바텀시트 펼침 상태시 패스
-            if(gap < 0 && bsIngH < bsMaxH){// gap이 마이너스 값임 && 드래그시 bsMaxH까지 높이 적용
-                $bs.height(bsIngH);
+            var gap = lastTouch - bottom_touch_start;
+            var bsIngH = bottomSheetH + (-gap);
+            
+
+            if(bottomSheet.hasClass('active')) return;
+            if(gap < 0 && bsIngH < bottomSheetH2){//gap이 마이너스 값임. 두번째 조건은 active 높이값만큼으로 변경 필요
+                bottomSheet.height(bsIngH);
             }
+            // 다 올라갔을 땐 몬가 구분자를 추가하여 높이값 변경 막기
+            // 뭘로 구분하지..??
+            // 지정 높이만큼 올라가면 터치 함수를 off 해주야해. 그럼 닫는 건 어째...?
+            // 만약 지정 높이만큼 올라가면 lastTouch 업데이트 막기
+            // 또는 바텀시트 높이값 대입 막기
         });
     
-        $handle.on('touchend', function(e){
-            if((touchStart - touchLast) > 0 && touchLast != 0){// up
-                $bs.animate({
-                    'height' : bsMaxH
+        draghandle.on('touchend', function(e){
+            if((bottom_touch_start - lastTouch) > 0 && lastTouch != 0){// up
+                // console.log('열림 : ', bottom_touch_start - lastTouch);
+                // console.log('초기 : ' + bottom_touch_start + ' 마지막 : ' + lastTouch);
+    
+                bottomSheet.animate({
+                    'height' : 500
                 }, 200, "swing");
 
                 $('.dragging-bs').addClass('active');
-            }else if((touchStart - touchLast) < 0 && touchLast != 0){// down
-                $bs.animate({
-                    'height' : bsh
+
+            }else if((bottom_touch_start - lastTouch) < 0 && lastTouch != 0){// down
+                // console.log('접힘 : ', bottom_touch_start - lastTouch);
+                // console.log('초기 : ' + bottom_touch_start + ' 마지막 : ' + lastTouch);
+    
+                bottomSheet.animate({
+                    'height' : 150
                 }, 300, "swing");
 
                 $('.dragging-bs').removeClass('active');
-            }       
+            }   
+    
         });
 
         // 드래그 테스트
